fb5624b5e85739544b1a90016327dc0b05473bdc
248nonny <ronny.cravioto@gmail.com>
2025-04-06 18:24:39 -0600

    first commit!

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.rustup
+    pkgs.qemu            # optional, if you want to emulate
+    pkgs.gdb             # optional, for debugging
+    pkgs.cargo-binutils  # for objdump, nm, etc.
+    pkgs.llvmPackages.bintools
+    pkgs.pkgsCross.aarch64-embedded.buildPackages.gcc
+  ];
+
+  RUST_TARGET_PATH = ".";
+
+  shellHook = ''
+    echo "Welcome to the bare-metal Raspberry Pi Rust dev shell!"
+    export CARGO_TARGET_AARCH64_UNKNOWN_NONE_GNU_LINKER=aarch64-none-elf-gcc
+  '';
+}
